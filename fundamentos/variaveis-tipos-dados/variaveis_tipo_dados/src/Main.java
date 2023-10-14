8023a45c4186611b85a1dd3361ae9e819f07496b
Henrique Diniz <PedroDinizLuiz@gmail.com>
2023-10-14 20:34:10 -0300

    trilhando meu conhecimento em Java

---
+++ fundamentos/variaveis-tipos-dados/variaveis_tipo_dados/src/Main.java
@@ -0,0 +1,23 @@
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		int idade;
+		double salario, altura;
+		char genero;
+		String nome;
+		
+		idade = 30;
+		salario = 5800.5;
+		altura = 1.72;
+		genero = 'F';
+		nome ="Maria Silva";
+		
+		System.out.println("IDADE: " + idade);
+		System.out.println("SALARIO: " + salario);
+		System.out.println("ALTURA: " + altura );
+		System.out.println("GENERO: " + genero);
+		System.out.println("NOME: " + nome);
+	}
+}
